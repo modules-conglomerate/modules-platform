3ca88873661aa5bd8c6714dec82c9f70c06f3fa0
назрОС <102623319+nazrOS-softhard@users.noreply.github.com>
2026-06-02 06:43:51 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '*.supabase.co' },
+      { protocol: 'https', hostname: 'api.mapbox.com' },
     ],
+  },
+  env: {
+    NEXT_PUBLIC_MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
   },
 }
 
